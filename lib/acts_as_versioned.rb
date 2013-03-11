c0a70fb2bb8f3fef65852f79d636da8dfdca9f24
Jason Whitehorn <jason.whitehorn@gmail.com>
2013-03-11 08:18:23 -0500

    versioned_columns is not a list of strings

--- lib/acts_as_versioned.rb
+++ lib/acts_as_versioned.rb
@@ -326,7 +326,7 @@ module ActiveRecord #:nodoc:
         end
 
         def altered?
-          changed.map { |c| self.class.versioned_columns.include?(c) & !self.class.version_except_columns.include?(c) }.any?
+          changed.map { |c| self.class.versioned_columns.map(&:name).include?(c) & !self.class.version_except_columns.include?(c) }.any?
         end
 
         # Clones a model.  Used when saving a new version or reverting a model's version.
